ddee0670dd90dae39fa77996ae5e8f04cc60b96f
mister-Mandarin <andr.poluehktov@yandex.ru>
2023-06-28 18:48:11 +0300

    проект завершен

--- src/App.js
+++ src/App.js
@@ -5,16 +5,23 @@ import {useState} from 'react';
 
 function App() {
 
-  const [formSubmit, setFormSubmit] = useState('null');
+  const [formSubmit, setFormSubmit] = useState(null);
 
   const success = (data) => {
     setFormSubmit(data)
   }
 
   return (
-    <div className={styles.wrapper}>
-      {formSubmit === null ? <Form success={success}/> : <Success formSubmit={formSubmit}/>}
-    </div>
+    <>
+      <div className={styles.wrapper}>
+        {formSubmit === null ? <Form success={success}/> : <Success formSubmit={formSubmit}/>}
+        <div className={styles.attribution}>
+          Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank" rel="noreferrer">Frontend
+          Mentor</a>.
+          Coded by <a href="#">Your Name Here</a>.
+        </div>
+      </div>
+    </>
   );
 }
 
